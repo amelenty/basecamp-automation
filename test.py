2a56a4a2075ee8b73b3929ed96fb3748aba5777f
amelentyeva <ada.melentyeva@gmail.com>
2017-11-06 15:39:03 +0200

    Added Python calculator testing script and Chrome browser driver

---
+++ test.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+testcases = {
+	'19+35' : '54',
+	'77*13' : '1001',
+	'95/20' : '4.75',
+	'20-32' : '-12'
+}
+
+def myclick(c, driver):
+    if c == '*':
+        c = 'x'
+    driver.find_element(By.XPATH, "//input[@value='" + c + "']").click()
+    
+def runtestcase(key, driver):
+    for c in key:
+        myclick(c, driver)
+    myclick('=', driver)
+    assert driver.find_element_by_id("resultsbox").get_attribute('value') == testcases[key]
+    myclick('C', driver)
+
+browser = webdriver.Chrome()
+browser.get('file:///C:/Users/ada.melentyeva/Documents/BaseCamp/Automation/calc.html')
+
+for testcase in testcases:
+    runtestcase(testcase, browser)
+
+browser.quit()
